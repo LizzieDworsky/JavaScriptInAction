e4c8df1988d9537879aef866ca37f41578ae30f1
LizzieDworsky <amydworsky15@gmail.com>
2022-06-06 12:34:24 -0600

    removed excess lines on worksheet + added comments labelling out the tasks

--- worksheet.js
+++ worksheet.js
@@ -1,8 +1,18 @@
+// Learning Variables
 // Task 1: Days of the Week
 let dayOfWeek = "Monday";
-
 console.log(dayOfWeek);
-
 dayOfWeek = "Friday";
-
 console.log(`I can't wait for ${dayOfWeek}`);
+
+// Task 2: User Input
+
+// Learning Conditionals
+// Task 3: Meals
+
+//Task 4: Random Number
+
+// Learning Loops
+// Task 5: For Loop
+
+// Task 6: While Loop
