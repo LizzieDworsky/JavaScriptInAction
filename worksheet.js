2625feef3e77147d389ff45fdb87c1d3151e2c7c
LizzieDworsky <amydworsky15@gmail.com>
2022-06-06 16:10:04 -0600

    created the Magic Number function guessing game

--- worksheet.js
+++ worksheet.js
@@ -7,3 +7,26 @@ desktopItems.push("Infinity Gauntlet");
 for (let index = 0; index < desktopItems.length; index++) {
     console.log(desktopItems[index]);
 }
+
+// Task 9: Boss Fight
+function magicNum() {
+    let magicNumber = Math.floor(Math.random() * 101);
+    let guess = 0;
+    do {
+        guess = parseInt(prompt("What do you think the Magic Number is? "));
+        if (guess === magicNumber) {
+            console.log(`Congrats! ${guess} is the Magic Number!`);
+        } else if (guess > magicNumber && guess <= magicNumber + 10) {
+            console.log("Getting warmer! Too high!");
+        } else if (guess < magicNumber && guess >= magicNumber - 10) {
+            console.log("Getting warmer! Too low!");
+        } else if (guess < magicNumber) {
+            console.log("Too low!");
+        } else if (guess > magicNumber) {
+            console.log("Too high!");
+        } else {
+            console.log("I'm sorry we didn't get that.");
+        }
+    } while (guess !== magicNumber);
+}
+magicNum();
