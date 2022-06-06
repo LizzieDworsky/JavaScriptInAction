e2fd1736d7283b77a9fa59602a5cbf4a9899a3fa
LizzieDworsky <amydworsky15@gmail.com>
2022-06-06 14:36:42 -0600

    created two more functions that work together to create a user personalized concert location

--- worksheet.js
+++ worksheet.js
@@ -6,8 +6,23 @@ while (isInstructorAwesome) {
 }
 
 // Task 7: Functions
-function printMovieName(params) {
+function printMovieName() {
     let favoriteMovie = "Pride and Prejudice";
     console.log(favoriteMovie);
 }
 printMovieName();
+
+function userFavoriteBand() {
+    result = prompt("Please enter your favorite band: ");
+    return result;
+}
+let userFavBand = userFavoriteBand();
+console.log(userFavBand);
+
+function concertDisplay(musicalAct) {
+    myStreet = prompt("Please enter the street you live on: ");
+    console.log(
+        `It would be great if ${musicalAct} played a show on ${myStreet}`
+    );
+}
+concertDisplay(userFavBand);
